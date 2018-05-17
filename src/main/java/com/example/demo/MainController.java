f2ed7dbd88ba254695eef2985df8a99e4e0488a8
lijiande <lijiande@users.noreply.github.com>
2018-05-17 19:33:57 +0800

    增加de'mo

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -2,14 +2,17 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@RestController()
+@Controller
 public class MainController {
 
     @Autowired
@@ -25,5 +28,12 @@ public class MainController {
         map.put("threadNum", threadNum);
         map.put("num", num);
         return ResponseEntity.ok(map);
+    }
+
+    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    public String test(Model model,@RequestParam String a) {
+
+        model.addAttribute("a",a);
+        return "home";
     }
 }
